5cb5d3e1f12f7a4894b2806db4aab4caeb691ad1
Royce Chua <roycecha.dev@gmail.com>
2023-03-01 00:03:52 +0800

    Add alert and preparation for api call

--- src/screens/HistoryScreen.tsx
+++ src/screens/HistoryScreen.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View, Alert } from 'react-native';
 import React, { useState } from 'react';
 import SafeAreaContainer from '../components/SafeAreaContainer';
 import Spacer from '../components/Spacer';
@@ -9,7 +9,7 @@ import HistoryListItem from '../components/HistoryListItem';
 export type HistoryItem = {
     preview: string;
     result: string | number;
-}
+};
 
 const HistoryScreen = () => {
     const [historyList, setHistoryList] = useState<HistoryItem[]>([
@@ -17,20 +17,45 @@ const HistoryScreen = () => {
         // { preview: '18 + 43 x 59', result: 2555 },
     ]);
 
+    const handleClearHistory = async () => {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <SafeAreaContainer>
             <View style={[styles.container, { backgroundColor: '#292A2D' }]}>
                 <Spacer space={10} />
-                <Header title='History' rightPress={() => {}} />
+                <Header
+                    title='History'
+                    rightPress={() => {
+                        Alert.alert(
+                            'Clear History',
+                            "You're about to clear your entire history. Are you sure you want to do this?",
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => {},
+                                    style: 'cancel',
+                                },
+                                {
+                                    text: 'Confirm',
+                                    onPress: () => handleClearHistory(),
+                                },
+                            ]
+                        );
+                    }}
+                />
                 <Spacer space={10} />
                 <FlatList
                     data={historyList}
                     keyExtractor={(item, index) => index.toString()}
                     ListEmptyComponent={<EmptyHistoryPlaceholder />}
                     renderItem={({ item, index }) => {
-                        return (
-                            <HistoryListItem data={item} index={index} />
-                        );
+                        return <HistoryListItem data={item} index={index} />;
                     }}
                 />
             </View>
